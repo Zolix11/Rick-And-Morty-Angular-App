3fa182700724ccda62791a56ba0970a2388113fd
Zolix11 <gazkazoli@gmail.com>
2023-05-14 19:11:14 +0200

    fixes

--- src/app/components/character-card-characters-page/character-card-characters-page.component.ts
+++ src/app/components/character-card-characters-page/character-card-characters-page.component.ts
@@ -10,26 +10,28 @@ export class CharacterCardCharactersPageComponent implements OnInit {
   @Input() character: Character = {} as Character;
   liked: boolean = false;
   constructor(private favoriresService: FavoriteCharactersService) {}
-
   ngOnInit() {
     this.checkIfLiked();
-    this.favoriresService.getRemovalSubject().subscribe((removedCharacter: Character) => {
-      if (removedCharacter.id === this.character.id) {
-        this.liked = false;
-      }
-    });
+    this.favoriresService
+      .getRemovalSubject()
+      .subscribe((removedCharacter: Character) => {
+        if (removedCharacter.id === this.character.id) {
+          this.liked = false;
+          console.log("Removing",this.liked);
+
+        }
+      });
   }
-  
+
   toggleLiked() {
     if (this.liked) {
       this.favoriresService.removeFromFavorites(this.character);
-    }
-    else {
+    } else {
       this.favoriresService.addToFavorites(this.character);
       this.liked = !this.liked;
+      console.log("Add",this.liked);
     }
   }
-  
 
   checkIfLiked() {
     this.liked = this.favoriresService.isCharacterFavorite(this.character);
